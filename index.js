0dcb09dc52b9d99eb532106e1f0e0584aab1b605
Jo Cruise <1973851482@qq.com>
2024-03-02 23:20:12 +0800

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+const firstList = [
+    "Expanding Cards",
+    "Progress Steps",
+    "WeChat Mini App DEMO",
+    "Rotating Navigation"
+];
+const groupList1 = document.querySelector('.collapse1').querySelector('.list-group');
+
+firstList.forEach(item => {
+    let itemName = item.toLowerCase().replace(/ /g, '-');
+    let itemList = document.createElement('li');
+    let listIcon = document.createElement('i');
+    let listLink = document.createElement('a');
+    itemList.classList.add('list-group-item');
+    listIcon.classList.add('fa', 'far', 'fa-file');
+    listLink.classList.add('mx-2');
+    listLink.target = '_blank';
+    listLink.href = '/'+ itemName; 
+    listLink.innerText = item;
+    itemList.append(listIcon, listLink);
+    groupList1.appendChild(itemList);
+
+    
+})
