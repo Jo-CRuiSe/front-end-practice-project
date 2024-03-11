c1affdbc483edfa81762b12dd9149a406da1fdb4
Jo Cruise <1973851482@qq.com>
2024-03-11 10:43:23 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const firstList = [
     "Expanding Cards",
     "Progress Steps",
-    "WeChat Mini App DEMO",
     "Rotating Navigation",
     "Hidden Search Widget",
 ];
+
+const secondList = [
+    "WeChat Mini App DEMO",
+];
+
 const groupList1 = document.querySelector('.collapse1').querySelector('.list-group');
+const groupList2 = document.querySelector('.collapse2').querySelector('.list-group');
+
 
 firstList.forEach(item => {
     let itemName = item.toLowerCase().replace(/ /g, '-');
@@ -21,6 +27,22 @@ firstList.forEach(item => {
     itemList.append(listIcon, listLink);
     groupList1.appendChild(itemList);
 })
+
+secondList.forEach(item => {
+    let itemName = item.toLowerCase().replace(/ /g, '-');
+    let itemList = document.createElement('li');
+    let listIcon = document.createElement('i');
+    let listLink = document.createElement('a');
+    itemList.classList.add('list-group-item');
+    listIcon.classList.add('fa', 'far', 'fa-file');
+    listLink.classList.add('mx-2');
+    listLink.target = '_blank';
+    listLink.href = '/front-end-practice-project/'+ itemName; 
+    listLink.innerText = item;
+    itemList.append(listIcon, listLink);
+    groupList2.appendChild(itemList);
+})
+
 
 document.querySelectorAll('.collapse1').forEach(collapse => {
     document.querySelectorAll('.catogry-trigger').forEach(trigger => {
